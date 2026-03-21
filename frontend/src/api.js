4561c0e0c8ed86b52fc46f3c580a04d9807c8a29
Gaurav-Gade-eng <gadegaurav10@gmail.com>
2026-03-21 17:49:53 +0530

    connected frontend to backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "http://localhost:5000/api"
+ baseURL: "https://placement-backend-4n9z.onrender.com/"
 });
 
 API.interceptors.request.use((req) => {
